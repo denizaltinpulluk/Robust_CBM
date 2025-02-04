43b9114ead84d7a8605184d164c7136528298c48
denizaltinpulluk <64498808+denizaltinpulluk@users.noreply.github.com>
2025-02-04 11:10:07 -0500

    Add files via upload

---
+++ Experiments.py
@@ -0,0 +1,89 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_results(experiments):
+    robus_objs = []
+    sim_obj_means = []
+    budget = []
+    num_failure = []
+    penalty = []
+    for exp in experiments:
+        budget.append(exp.budget)
+        robus_objs.append(exp.robust_obj)
+        sim_obj_means.append(np.mean(exp.simulation_objs))
+        num_failure.append(np.mean(exp.simulation_failure))
+        penalty.append(np.mean(exp.simulation_penalty))
+
+    fig = plt.figure()
+    plt.plot(budget, sim_obj_means, label=' Sim. Ave')
+    plt.plot(budget, robus_objs, label=' robust')
+    plt.xlabel("Budget")
+    plt.ylabel("Obj F. Value")
+    plt.legend()
+    plt.show()
+
+
+    fig = plt.figure()
+    plt.plot(budget, penalty, label=' Sim. Ave')
+    plt.xlabel("Budget")
+    plt.ylabel("Total Penalty")
+    plt.legend()
+    plt.show()
+
+
+
+    fig = plt.figure()
+    plt.plot(budget, num_failure, label=' Sim. Ave')
+    plt.xlabel("Budget")
+    plt.ylabel("Number of Failures")
+    plt.legend()
+    plt.show()
+
+    #plt.savefig('900_70_demand.jpg')
+class Experiments:
+    def __init__(self,results):
+        self.name = results['experiment_name']
+        self.budget = results['budget']
+        self.robust_obj = results['robust_obj']
+        self.simulation_objs = results['simulations']
+        self.simulation_penalty= results['penalty']
+        self.simulation_failure = results['failure']
+
+
+
+def create_experiments_from_files():
+    path = "results"
+    files = os.listdir(path)
+    experiments = []
+
+    for file in files:
+        results = {}
+        separator = file.split("_")
+        results['experiment_name'] = separator[1]
+        results['budget'] = float(separator[2][0:4])
+        path_to_file = path + '/' + file
+        simulation_objs = []
+        simulation_failures =[]
+        simulation_penalty = []
+        with open(path_to_file) as f:
+            contents = f.readlines()
+        lines = []
+        for content in contents:
+            line = content.split()
+            lines.append(line)
+        results['robust_obj'] = float(lines[0][3])
+        for i in range(2,len(lines)):
+            simulation_objs.append(float(lines[i][2]))
+            simulation_penalty.append(float(lines[i][3]))
+            simulation_failures.append(int(lines[i][4]))
+        results['simulations'] = np.array(simulation_objs)
+        results['penalty'] = np.array(simulation_penalty)
+        results['failure'] = np.array(simulation_failures)
+        experiments.append(Experiments(results))
+    return experiments
+if __name__ == '__main__':
+    experiments = create_experiments_from_files()
+    plot_results(experiments)
+    print()
